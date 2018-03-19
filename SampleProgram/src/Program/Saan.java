01aa6a53251249d5f9f2bed4bc58cfb10c01b8ee
balaji.bhadre85 <balaji.bhadre85@gmail.com>
2018-03-19 22:56:02 +0530

    added from eclipse

---
+++ SampleProgram/src/Program/Saan.java
@@ -0,0 +1,7 @@
+package Program;
+
+
+public class Saan extends Thread {
+
+	// this is balaji
+}
---
+++ SampleProgram/src/Program/xyz.java
@@ -0,0 +1,10 @@
+package Program;
+
+public class xyz {
+
+	public xyz() throws CloneNotSupportedException
+	{
+		xyz x=new xyz();
+		x.clone();
+	}
+}
